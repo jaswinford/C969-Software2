58df3ce6660398bff9eb4e896ad4cd18baac4561
Adam Swinford <aswinford@jadam.space>
2025-05-12 18:17:17 -0400

    feat: add initial Customer boilerplate

---
+++ customer_scheduler/customer.cs
@@ -0,0 +1,19 @@
+namespace customer_scheduler
+{
+    /// <summary>
+    /// Customer object and logic to interact with DB for customer records
+    /// </summary>
+    ///
+    /// TODO: Add member variables
+    /// TODO: 'Add' Function
+    /// TODO: 'Update' Function
+    /// TODO: 'Delete' Function
+    /// TODO: 'Validate' Function
+    public class Customer
+    {
+        public Customer()
+        {
+
+        }
+    }
+}
