3950c1d1fb29cd64f608bd25d9fc5bfb73494cb6
Adam Swinford <aswinford@jadam.space>
2025-05-12 18:04:43 -0400

    feat: Added Login Window form

---
+++ customer_scheduler/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace customer_scheduler
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    /// 
+    /// TODO: Determin users' location
+    /// TODO: Translate login and error control messages into English and one additional language
+    /// TODO: Verify credentials.
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            // TODO : Get user's location on window initialization
+        }
+    }
+}
