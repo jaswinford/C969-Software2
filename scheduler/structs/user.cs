246b040b01c9e0218415515f857d7c5e1e5ab5cf
jaswinford <aswinford@jadam.space>
2025-05-20 00:57:30 -0400

    `feat: add password authentication for user with database validation`

--- scheduler/structs/user.cs
+++ scheduler/structs/user.cs
@@ -1,3 +1,7 @@
+using System.Windows;
+using MySql.Data.MySqlClient;
+using scheduler.database;
+
 namespace scheduler.structs
 {
     /// <summary>
@@ -12,14 +16,33 @@ namespace scheduler.structs
         public bool IsActive { get; set; } = true; // active TINYINT
         // We don't store the user password, if at all possible.
 
-        public bool Authenticated
+        public bool Authenticated(string password)
         {
-            get
-            {
-                if (!IsActive) return false;
+            if (!IsActive) return false;
 
-                return false;
+            try
+            {
+                DatabaseManager.Instance.Connect();
+                using (var cmd = new MySqlCommand("SELECT password FROM user WHERE userName == '" + Name + "'",
+                           DatabaseManager.Instance.Connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read()) return reader.GetString(0) == password;
+                    }
+                }
             }
+            catch (MySqlException ex)
+            {
+                LanguageManager.Instance.ShowMessageBox("Message.SQLError", "Title.SQLError", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+            finally
+            {
+                DatabaseManager.Instance.Disconnect();
+            }
+
+            return false;
         }
     }
 }
