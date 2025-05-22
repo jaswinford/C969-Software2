f98a09936952752f4771a7a4894512785244ddf1
jaswinford <aswinford@jadam.space>
2025-05-22 01:41:04 -0400

    refactor(address): add validation and sanitization logic for Address properties

--- scheduler/structs/address.cs
+++ scheduler/structs/address.cs
@@ -1,16 +1,64 @@
+using System.Text.RegularExpressions;
+using scheduler.database;
+
 namespace scheduler.structs
 {
     /// <summary>
     /// Address object and logic to interact with DB for address records
     /// </summary>
-    /// <remarks>
-    /// </remarks>
-    public partial class Address : DBObject
+    public class Address : DBObject
     {
-        public string Address1 { get; set; } = string.Empty; // address VARCHAR(50)
-        public string Address2 { get; set; } = string.Empty; // address2 VARCHAR(50)
-        public City City { get; set; } = new City(); // cityId INT(10)
-        public string PostalCode { get; set; } = string.Empty; // postalCode VARCHAR(10)
-        public string Phone { get; set; } = string.Empty; // phone VARCHAR(20)
+        private string _address1;
+        private string _address2;
+        private string _phone;
+        private string _postalCode;
+
+        public string Address1
+        {
+            get => _address1;
+            set => _address1 = DatabaseManager.SanitizeString(value, 50);
+        } // address VARCHAR(50)
+
+        public string Address2
+        {
+            get => _address2;
+            set => _address2 = DatabaseManager.SanitizeString(value, 50);
+        } // address2 VARCHAR(50)
+
+        public City City { get; set; } // cityId INT(10)
+
+        public string PostalCode
+        {
+            get => _postalCode;
+            set => _postalCode = DatabaseManager.SanitizeString(value, 10);
+        } // postalCode VARCHAR(10)
+
+        public string Phone
+        {
+            get => _phone;
+            set => _phone = DatabaseManager.SanitizeString(value, 20);
+        } // phone VARCHAR(20)
+
+        /// <summary>
+        ///     Determines whether the current address instance satisfies defined validation rules.
+        /// </summary>
+        /// <remarks>
+        ///     A.2.a - Validation is based on the following criteria:
+        ///     - The `Address1`, `City.Name`, `PostalCode`, and `Phone` fields must be non-empty.
+        ///     - The `Phone` field must match a valid pattern that permits only digits and dashes.
+        /// </remarks>
+        /// <returns>
+        ///     Returns true if the address is valid according to the specified conditions; otherwise, false.
+        /// </returns>
+        public bool IsValid
+        {
+            get
+            {
+                if (_address1 == string.Empty || City.Name == string.Empty || _postalCode == string.Empty ||
+                    _phone == string.Empty) return false; //verify address is provided
+                if (!Regex.IsMatch(_phone, @"^[0-9-]+$")) return false;
+                return true;
+            }
+        }
     }
 }
