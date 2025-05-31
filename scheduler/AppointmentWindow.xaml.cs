9be53da0175d7614e2f027f785ef4e42dbbf816d
Adam Swinford <aswinford@jadam.space>
2025-05-30 21:23:29 -0700

    feat: Add Appointment and Customer window implementations

---
+++ scheduler/AppointmentWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace scheduler
+{
+    public partial class AppointmentWindow : Window
+    {
+        public AppointmentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ scheduler/CustomerWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace scheduler
+{
+    public partial class CustomerWindow : Window
+    {
+        public CustomerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
